175e8ed334119be01da91a4424baab1aed1325d0
Daniel Ungureanu <leinad.ungureanu@gmail.com>
2022-03-07 18:49:31 +0000

    App finished and tested.

--- employee_data_entry.py
+++ employee_data_entry.py
@@ -1,1 +1,44 @@
+# variable to capture number of employees as string
+number_of_employees_str = input("Please enter the number of employees: ")
 
+# var to save number of employees as integer
+num_of_emp = int(number_of_employees_str)
+
+# variable to save employees details
+list_of_employees = {}
+
+# variable to sae the sum of all ages
+sum_of_ages = 0
+
+# loop to capture all employees details
+for i in range(num_of_emp):
+    # capture first name and check it is not empty
+    first_name = input("Please enter the first name: ")
+    while len(first_name) == 0:
+        first_name = input("Please enter a valid first name: ")
+
+    # capture last name and check it is not empty
+    last_name = input("Please enter the last name: ")
+    while len(last_name) == 0:
+        last_name = input("Please enter a valid last name: ")
+
+    # capture age, cast it to integer and check it is in 18 to 100 range
+    age = int(input("Please enter the employee's age: "))
+    while (age < 18) or (age > 100):
+        age = int(input("Please enter an age in the 18 to 100 range: "))
+
+    # add employees to dict using value of iterator as uniques key
+    list_of_employees[i] = {"firstname": first_name, "lastname": last_name, "age": age}
+
+    sum_of_ages += age
+
+# format employees details print output
+for entry in list_of_employees.values():
+    print(f'{entry["firstname"]} {entry["lastname"]} is {entry["age"]} years old.')
+
+# debug warning msg
+# sum_of_ages=600
+
+# check if sum of all ages is over 500
+if sum_of_ages > 500:
+    print("WARNING!!! The sum of all ages is over 500!!!")
