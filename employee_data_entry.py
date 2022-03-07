d4cc53497400d250deac29ce817a1948cb64b28f
Daniel Ungureanu <leinad.ungureanu@gmail.com>
2022-03-07 19:03:33 +0000

    Fixed check on number of employees check. Replace if with while.

--- employee_data_entry.py
+++ employee_data_entry.py
@@ -1,6 +1,6 @@
 # variable to capture number of employees as string
 number_of_employees_str = input("Please enter the number of employees: ")
-if not number_of_employees_str.isdigit():
+while not number_of_employees_str.isdigit():
     number_of_employees_str = input("Please enter a valid number of employees: ")
 
 # var to save number of employees as integer
