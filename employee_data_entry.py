7f5a1fe0be1cee1830b50bc6334b1a4178f36efb
Daniel Ungureanu <leinad.ungureanu@gmail.com>
2022-03-08 10:03:59 +0000

    Added strip to names inputs.

--- employee_data_entry.py
+++ employee_data_entry.py
@@ -15,14 +15,14 @@ sum_of_ages = 0
 # loop to capture all employees details
 for i in range(num_of_emp):
     # capture first name and check it is not empty
-    first_name = input("Please enter the first name: ").capitalize()
+    first_name = input("Please enter the first name: ").strip().capitalize()
     while len(first_name) == 0:
-        first_name = input("Please enter a valid first name: ").capitalize()
+        first_name = input("Please enter a valid first name: ").strip().capitalize()
 
     # capture last name and check it is not empty
-    last_name = input("Please enter the last name: ").capitalize()
+    last_name = input("Please enter the last name: ").strip().capitalize()
     while len(last_name) == 0:
-        last_name = input("Please enter a valid last name: ").capitalize()
+        last_name = input("Please enter a valid last name: ").strip().capitalize()
 
     # capture age, cast it to integer and check it is in 18 to 100 range
     age_str = input("Please enter the employee's age: ")
