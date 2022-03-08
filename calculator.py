7cb8189a6f9fd59038a2678eb255609b7707c5c8
Daniel Ungureanu <leinad.ungureanu@gmail.com>
2022-03-08 16:31:27 +0000

    Finished calculator.

--- calculator.py
+++ calculator.py
@@ -1,25 +1,58 @@
-def numbers_sum(a, b):
-    return a + b
-
-
-def numbers_sub(a, b):
-    return a - b
-
-
-def numbers_div(a, b):
-    if b != 0:
-        return a / b
-
-
-def numbers_mul(a, b):
-    return a * b
-
-def read_numbers():
+def read_first_number():
     while True:
         first_number_str = input("Please enter the first number: ")
         if first_number_str.isdigit():
-            first_number = int()
+            first_number = int(first_number_str)
+            return first_number
+        else:
+            print("Please enter a valid first number: ")
+
+
+def read_second_number():
+    while True:
+        second_number_str = input("Please enter the second number: ")
+        if second_number_str.isdigit():
+            second_number = int(second_number_str)
+            return second_number
+        else:
+            print("Please enter a valid second number: ")
+
+
+def numbers_op(op_sign, a, b):
+    result = 0
+    if op_sign == "+":
+        result = a + b
+    elif op_sign == "-":
+        result = a - b
+    elif op_sign == "*":
+        result = a * b
+    elif op_sign == "/":
+        result = a / b
+    else:
+        print("Unknown operation!")
+    return result
+
+
+def read_operation():
+    allowed_operations = ["+", "-", "*", "\\", "quit"]
+    while True:
+        operation = input("Please enter any of the following operations +, -, *, / : ")
+        if operation in allowed_operations:
+            return operation
+        else:
+            print("Unknown operation.")
 
 
 if __name__ == '__main__':
-    print("this is main body")
+    op = ""
+    while True:
+        op = read_operation()
+        if op.lower() == "quit":
+            print("\nExiting calculator...")
+            break
+        num1 = read_first_number()
+        num2 = read_second_number()
+        if op == "/" and num2 == 0:
+            print("Division by zero not possible!")
+        else:
+            print(f"{num1} {op} {num2} = {numbers_op(op, num1, num2)}")
