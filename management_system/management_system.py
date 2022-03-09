7b5141427947cac5b7856739b09d4d3dac76a1eb
Daniel Ungureanu <leinad.ungureanu@gmail.com>
2022-03-09 11:35:00 +0000

    management system WIP

---
+++ management_system/management_system.py
@@ -0,0 +1,9 @@
+import employees
+
+if __name__ == '__main__':
+    print("This is the management_system.py")
+    employees.read_firstname()
+    employees.read_lastname()
+    employees.read_birth_yeah()
+    employees.read_birth_month()
+    employees.read_birth_day()
